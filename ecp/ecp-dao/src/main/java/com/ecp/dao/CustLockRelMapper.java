df4086b05bdcb3de7e8151013f43a88eeac9d3e5
lenovo <lenovo@Hz>
2018-03-18 23:51:34 +0800

    签约客户绑定DAO层

--- ecp/ecp-dao/src/main/java/com/ecp/dao/CustLockRelMapper.java
+++ ecp/ecp-dao/src/main/java/com/ecp/dao/CustLockRelMapper.java
@@ -1,7 +1,15 @@
 package com.ecp.dao;
 
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.ecp.entity.CustLockRel;
+
 import tk.mybatis.mapper.common.Mapper;
 
 public interface CustLockRelMapper extends Mapper<CustLockRel> {
+	public List<Map<String,Object>> getSalesByAgentId(@Param("agentId") long agentId,@Param("roleName")String roleName);
+	public List<Map<String,Object>> getSales(@Param("roleName") String roleName);
 }
