4f7b195d05bfdf670988c0394b91a22431c54621
srd <870306552@qq.com>
2018-03-27 22:45:34 +0800

    增加角色编码常量

---
+++ ecp/ecp-back/src/main/java/com/ecp/back/commons/RoleCodeConstants.java
@@ -0,0 +1,12 @@
+package com.ecp.back.commons;
+
+public class RoleCodeConstants {
+
+	public static final String ADMIN = "ADMIN";
+	public static final String IS = "IS";
+	public static final String OS = "OS";
+	public static final String BUSSMAN = "BUSSMAN";
+	public static final String SALEMAN = "SALEMAN";
+	public static final String MANAGER = "MANAGER";
+	
+}
