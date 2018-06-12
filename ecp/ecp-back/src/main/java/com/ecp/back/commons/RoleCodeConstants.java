dc4dd492b30ee6c90cf9b8fc2879564704199fb6
lenovo <lenovo@Hz>
2018-06-12 11:40:21 +0800

    角色常量:增加角色编码-角色名称注解.

--- ecp/ecp-back/src/main/java/com/ecp/back/commons/RoleCodeConstants.java
+++ ecp/ecp-back/src/main/java/com/ecp/back/commons/RoleCodeConstants.java
@@ -2,11 +2,11 @@ package com.ecp.back.commons;
 
 public class RoleCodeConstants {
 
-	public static final String ADMIN = "ADMIN";
-	public static final String IS = "IS";
-	public static final String OS = "OS";
-	public static final String BUSSMAN = "BUSSMAN";
-	public static final String SALEMAN = "SALEMAN";
-	public static final String MANAGER = "MANAGER";
+	public static final String ADMIN = "ADMIN";   	//系统管理员
+	public static final String IS = "IS";		  	//内部销售  outside sales
+	public static final String OS = "OS";			//外部销售 inside sales
+	public static final String BUSSMAN = "BUSSMAN"; //商务经理 business manager	
+	public static final String SALEMAN = "SALEMAN"; //销售经理 sales manager
+	public static final String MANAGER = "MANAGER"; //总经理   
 	
 }
